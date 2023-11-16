c63cb56d556f5b05540083e088da7552a7da6765
chunyoupeng <chunyoupeng@gmail.com>
2023-11-16 15:46:32 +0800

    add test_llm

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -3,6 +3,8 @@ llm = ChatOpenAI(
     openai_api_base="https://aiapi.xing-yun.cn/v1",
     openai_api_key="sk-3e5wTBAl2iFDvQvW9b5693C90a97425eBf3b4bEa558eC66a",
     temperature=0.1,
+   
+   
     model_name="gpt-3.5-turbo",
     # streaming=True,  # ! important
     # callbacks=[StreamingStdOutCallbackHandler()]  # ! important
